dd7f45da1835e1d45dc71c692fd4952cb55e3d08
Macrophage6 <85226632+Macrophage6@users.noreply.github.com>
2024-05-13 19:37:59 +0200

    added pfad + weg

--- build/classes/java/main/com/piske/piske/weg.java
+++ build/classes/java/main/com/piske/piske/weg.java
@@ -2,5 +2,17 @@ public class weg {
  pfad head = null;
  pfad tail = null;
 
- public void weg()
+ public void weg() {
+ }
+
+ public void appendPfad(char s, char e, int x, int y){
+   pfad temp = new pfad(s,e,x,y);
+   if(head == null) {
+    head = temp;
+    tail = temp;
+   } else {
+      temp.setPrevious(tail);
+      tail.setNext(temp);
+   }
+ }
 }
