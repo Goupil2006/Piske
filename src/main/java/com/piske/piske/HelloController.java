90294aaf71235d6fae2dfa5034864c9b61e823c0
Goupil2000 <60356885+Goupil2006@users.noreply.github.com>
2024-05-13 12:16:48 +0200

    init

---
+++ src/main/java/com/piske/piske/HelloController.java
@@ -0,0 +1,14 @@
+package com.piske.piske;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
