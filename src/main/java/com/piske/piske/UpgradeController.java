dc7b74f2625659daee3d3154745f887d85050ccb
Macrophage6 <85226632+Macrophage6@users.noreply.github.com>
2024-06-03 12:06:08 +0200

    Schülerimages

--- src/main/java/com/piske/piske/UpgradeController.java
+++ src/main/java/com/piske/piske/UpgradeController.java
@@ -1,20 +1,13 @@
 package com.piske.piske;
 
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.function.Consumer;
 
 public class UpgradeController implements Initializable {
 
